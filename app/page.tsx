0ae77319a464cbb962dd0317dd9a6fbe0ba09ada
S M Sanbim Sifat <79338515+sms-Sifat-115@users.noreply.github.com>
2024-10-05 09:24:40 +0600

    7th Draft: Projects Card Added

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Grid from "@/components/Grid";
 // import Clients from "@/components/Clients";
 // import Approach from "@/components/Approach";
 // import Experience from "@/components/Experience";
-// import RecentProjects from "@/components/RecentProjects";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
@@ -18,8 +18,8 @@ const Home = () => {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
-        {/* <RecentProjects />
-        <Clients />
+        <RecentProjects />
+        {/* <Clients />
         <Experience />
         <Approach />
         <Footer /> */}
