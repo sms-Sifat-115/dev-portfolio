9858ad0c83e768bef0a1844f7d8c0e91a6a7bfe5
S M Sanbim Sifat <79338515+sms-Sifat-115@users.noreply.github.com>
2024-10-05 07:52:26 +0600

    2nd Draft: Hero Section Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 export default function Home() {
   return (
-   <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-    <div  className="max-w-7xl w-full">
-      <h1>Hello World</h1>
-    </div>
-   </main>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        {/* <FloatingNav navItems={navItems} /> */}
+        <Hero />
+      </div>
+    </main>
   );
 }
