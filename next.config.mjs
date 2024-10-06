cee6a332a89306790f517b29890ee5bd4f5c4aa5
S M Sanbim Sifat <79338515+sms-Sifat-115@users.noreply.github.com>
2024-10-06 19:35:14 +0600

    19th Draft: config file amended

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/dev-portfolio/" : "",
+};
 
 export default nextConfig;
